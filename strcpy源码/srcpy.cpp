e4d046c3b58efc13cd4aee1e8ee20da93becf190
lihuawei <1206909328@qq.com>
2019-03-27 11:22:21 +0800

    stcpy

---
+++ strcpy源码/srcpy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+char* strcpy(char* dst, const char* src)
+{
+    
+}
+
+int main()
+{
+    return 0;
+}
---
+++ 反转链表/interseetlist.cpp
@@ -0,0 +1,14 @@
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+    }
+};
