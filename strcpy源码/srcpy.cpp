76c3b3112082e30484dcd8f68aca4e03a6b6fdb9
LAPTOP-O7SF1FPL\12069 <1206909328@qq.com>
2019-03-27 14:27:53 +0800

    strcpy

--- strcpy源码/srcpy.cpp
+++ strcpy源码/srcpy.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+// #include <Streams.h>
+// #include <Wxdebug.h>
+#include <assert.h>
 
 char* strcpy(char* dst, const char* src)
 {
-    
+    assert((dst != nullptr) || (src != nullptr));
+
+    char* dest = dst;
+
+    while((*dst++ = *src++) != '\0')
+    {
+
+    }
+
+    return dest;
+
 }
 
 int main()
 {
+    // std::cout<< "helloworld" <<std::endl;
+    char src[100] = "helloworld";
+    char the[101];
+    strcpy(the,src);
+    std::cout<<*the<<std::endl;
     return 0;
 }
