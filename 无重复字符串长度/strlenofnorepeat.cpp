f522d07e2bc05e438b729ba1f5790c870cb46057
lihuawei <1206909328@qq.com>
2019-03-31 09:21:49 +0800

    mark

---
+++ 无重复字符串长度/strlenofnorepeat.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        //auto len = s.length()
+		foreach(tr,s){
+			cout<< tr;
+		}
+    }
+};
+
+int main(){
+	Solution a;
+	a.lengthOfLongestSubstring("aaabbbccc");
+	return 0;
+}
